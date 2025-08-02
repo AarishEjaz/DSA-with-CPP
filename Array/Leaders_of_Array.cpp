b3a0e4223a710d77972be2ad7a46ca28723139e2
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-08-02 11:51:57 +0530

    added a  code

---
+++ Array/Leaders_of_Array.cpp
@@ -0,0 +1,15 @@
+void leaders(int arr[], int n ){
+    int curr_leader = arr[n-1];
+
+    for(int i=n; i>=0; i--){
+        if(curr_leader < arr[i]){
+            curr_leader = arr [i];
+            cout<< curr_leader;
+        }
+    }
+}
+
+// we will set the (n-1)th element as curr_leader
+// we will now start traversing
+// if we encounter a element greater than curr_leader then we will assign curr_leader = that element
+// we will print along the curr_leaders
