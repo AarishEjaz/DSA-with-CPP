78b29fda728943f0fe921beec28044802bf105aa
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-08-07 16:24:21 +0530

    added approach

--- Array/Maximum_consecutive1.cpp
+++ Array/Maximum_consecutive1.cpp
@@ -13,3 +13,14 @@ int maxConsecutiveOne(bool arr[], int n){
     }
     return res;
 }
+
+// we have a array filled with 0 and 1
+// we have to find the maximum count of consecutive 1
+
+// we will declare two variables curr and res
+
+// if we come to an element that is not 1 while traversing
+// we will reset the curr with 0
+
+// else we will keep incrementing the curr
+// and set the value equal to the max of res and curr
