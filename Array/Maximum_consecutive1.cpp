91592c7494917b02d358ec3352fd3c870830af63
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-08-07 16:18:01 +0530

    added code

---
+++ Array/Maximum_consecutive1.cpp
@@ -0,0 +1,15 @@
+int maxConsecutiveOne(bool arr[], int n){
+    res = 0;
+    curr = 0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i]==0){
+            curr=0;
+        }else{
+            curr++;
+            res = max(res,curr);
+        }
+
+    }
+    return res;
+}
